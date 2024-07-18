a31bd9d1d50cded47d1179b236d9df8e2982ebc6
eichc <eich.cameron@gmail.com>
2024-07-18 16:33:20 -0400

    setting up loop

--- part1/main.cpp
+++ part1/main.cpp
@@ -1,5 +1,11 @@
 #include <string>
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+extern vector<string> convertProcesses(int p);
+extern double next_exp(double lambda, int upperBound);
 
 int main(int argc, char *argv[]) {
     //get command line args
@@ -9,6 +15,12 @@ int main(int argc, char *argv[]) {
     double lambda = atoi(*(argv+4));
     int upperBound = atoi(*(argv+5));
     
-    //initialize random number generator
+    //initialize random number generator and process ids
     srand48(seed);
+    vector<string> convertProcesses(n);
+
+    //run loop for each process to assign arrival, burst, etc.
+    for (int i = 0; i < n; i++) {
+
+    }
 }
