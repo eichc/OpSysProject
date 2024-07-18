13ae27b02db50471b1c27e422e52916b50266ad7
eichc <eich.cameron@gmail.com>
2024-07-18 16:21:31 -0400

    next_exp

--- part1/main.cpp
+++ part1/main.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+    //get command line args
     int n = atoi(*(argv+1));
     int nCPU = atoi(*(argv+2));
-    int seed = atoi(*(argv+3));
+    unsigned int seed = atoi(*(argv+3));
     double lambda = atoi(*(argv+4));
     int upperBound = atoi(*(argv+5));
     
+    //initialize random number generator
+    srand48(seed);
 }
---
+++ part1/next_exp.cpp
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+double next_exp(double lambda, int upperBound) {
+    double x = INT_MAX;
+    while (x > upperBound) {
+        double r = drand48();
+        x = -log(r) / lambda;
+    }
+    return x;
+}
