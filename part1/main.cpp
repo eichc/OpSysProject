8225da30ddda67dab82c39a32699e0e3cf2c09f1
Satyam Patel <117528774+SatyamPatelCode@users.noreply.github.com>
2024-07-18 15:45:30 -0400

    Update main.cpp

--- part1/main.cpp
+++ part1/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    int n = *(argv+1);
+    int n = atoi(*(argv+1));
     int nCPU = atoi(*(argv+2));
     int seed = atoi(*(argv+3));
     double lambda = atoi(*(argv+4));
