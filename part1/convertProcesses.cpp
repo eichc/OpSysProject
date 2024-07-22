cce93505538ce8e80048ff3273012d4b7b00e27b
Satyam Patel <117528774+SatyamPatelCode@users.noreply.github.com>
2024-07-22 17:01:47 -0400

    simout output file

--- part1/convertProcesses.cpp
+++ part1/convertProcesses.cpp
@@ -9,8 +9,8 @@ std::vector<std::string> convertProcesses(int p) {
     std::vector<std::string> pID;
     int counter=0;
     for (int i=65; i<=90; i++) {
-        char letter = static_cast<char>(i);
-        for (int j=0; i<=9; j++) {
+        char letter = char(i);
+        for (int j=0; j<=9; j++) {
             char number_char = '0'+j;
             std::string result;
             result+=letter;
---
+++ part1/printSimout.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+void printSimout(int p, int CPUp, int IOp, double CPUavgCPUBurst, double IOavgCPUBurst, double avgCPUBurst, double CPUavgIOBurst, double IOavgIOBurst, double avgIOBurst) {
+    ofstream SimOut("simout.txt");
+    SimOut << "-- number of processes: " << p << endl;
+    SimOut << "-- number of CPU-bound processes: " << CPUp << endl;
+    SimOut << "-- number of I/O-bound processes: " << IOp << endl;
+    SimOut << "-- CPU-bound average CPU burst time: " << setprecision(4) << CPUavgCPUBurst <<" ms" << endl;
+    SimOut << "-- IO-bound average CPU burst time: " << setprecision(4) << IOavgCPUBurst <<" ms" << endl;
+    SimOut << "-- overall average CPU burst time: " << setprecision(4) << avgCPUBurst << " ms" << endl;
+    SimOut << "-- CPU-bound average I/O burst time: " << setprecision(4) << CPUavgIOBurst << " ms" << endl;
+    SimOut << "-- I/O-bound average I/o burst time: " << setprecision(4) << IOavgIOBurst << " ms" << endl;
+    SimOut << "-- overall average I/O burst time: " << setprecision(4) << avgIOBurst << " ms" << endl;
+    SimOut.close();
+}
