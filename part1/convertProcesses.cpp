9fafe5b09e1ca3af8972e9075e6b71da3c22366a
eichc <eich.cameron@gmail.com>
2024-07-22 17:10:28 -0400

    added main loop

--- part1/convertProcesses.cpp
+++ part1/convertProcesses.cpp
@@ -5,16 +5,19 @@
 #include <fstream>
 #include <vector>
 
-std::vector<std::string> convertProcesses(int p) {
-    std::vector<std::string> pID;
+using namespace std;
+
+vector<string> convertProcesses(int p) {
+    vector<string> pID;
     int counter=0;
     for (int i=65; i<=90; i++) {
-        char letter = static_cast<char>(i);
-        for (int j=0; i<=9; j++) {
+        char letter = i;
+        for (int j=0; j<=9; j++) {
             char number_char = '0'+j;
-            std::string result;
+            string result;
             result+=letter;
             result+=number_char;
+            cout << result << endl;
             pID.push_back(result);
             counter++;
             if (counter==p) {
--- part1/main.cpp
+++ part1/main.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include "Process.h"
 
 using namespace std;
 
@@ -17,10 +19,37 @@ int main(int argc, char *argv[]) {
     
     //initialize random number generator and process ids
     srand48(seed);
-    vector<string> convertProcesses(n);
+    vector<string> pids = convertProcesses(n);
 
     //run loop for each process to assign arrival, burst, etc.
     for (int i = 0; i < n; i++) {
-        
+        bool cpuBound = true;
+        if (i > nCPU) {
+            cpuBound = false;
+        }
+        string id = pids[i];
+        int arrival = floor(next_exp(lambda, upperBound));
+        int bursts = ceil(drand48() * 32);
+        Process p(id, arrival, bursts, cpuBound);
+        cout << p.getId() << " " << p.getArrivalTime() << " " << p.getNumBursts() << " " << p.isCpuBound() << endl;
+        //generate each burst time
+        int cpuTime, ioTime;
+        for (int j = 0; j < bursts-1; j++) {
+            cpuTime = ceil(next_exp(lambda, upperBound));
+            ioTime = ceil(next_exp(lambda, upperBound));
+            if (cpuBound) {
+                cpuTime *= 4;
+            } else {
+                ioTime *= 8;
+            }
+            p.setCpuBurstTime(cpuTime);
+            p.setIOBurstTime(ioTime);
+        }
+        //add one more cpu burst
+        cpuTime = ceil(next_exp(lambda, upperBound));
+        if (cpuBound) {
+            cpuTime *= 4;
+        }
+        p.setCpuBurstTime(cpuTime);
     }
 }
