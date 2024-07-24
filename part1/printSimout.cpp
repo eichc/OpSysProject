56b592d86271567fffc5ce3932023cad5e105b9b
Satyam Patel <117528774+SatyamPatelCode@users.noreply.github.com>
2024-07-24 13:33:43 -0400

    Update printSimout.cpp

--- part1/printSimout.cpp
+++ part1/printSimout.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <iomanip>
 #include <fstream>
+#include <cstdio>
+#include <cmath>
 
 using namespace std;
 
@@ -11,11 +13,11 @@ void printSimout(int p, int CPUp, int IOp, double CPUavgCPUBurst, double IOavgCP
     SimOut << "-- number of processes: " << p << endl;
     SimOut << "-- number of CPU-bound processes: " << CPUp << endl;
     SimOut << "-- number of I/O-bound processes: " << IOp << endl;
-    SimOut << "-- CPU-bound average CPU burst time: " << setprecision(4) << CPUavgCPUBurst <<" ms" << endl;
-    SimOut << "-- IO-bound average CPU burst time: " << setprecision(4) << IOavgCPUBurst <<" ms" << endl;
-    SimOut << "-- overall average CPU burst time: " << setprecision(4) << avgCPUBurst << " ms" << endl;
-    SimOut << "-- CPU-bound average I/O burst time: " << setprecision(4) << CPUavgIOBurst << " ms" << endl;
-    SimOut << "-- I/O-bound average I/O burst time: " << setprecision(4) << IOavgIOBurst << " ms" << endl;
-    SimOut << "-- overall average I/O burst time: " << setprecision(4) << avgIOBurst << " ms" << endl;
+    SimOut << "-- CPU-bound average CPU burst time: " << (ceil((CPUavgCPUBurst *1000))/1000) <<" ms" << endl;
+    SimOut << "-- IO-bound average CPU burst time: " <<  (ceil((IOavgCPUBurst *1000))/1000) <<" ms" << endl;
+    SimOut << "-- overall average CPU burst time: " << (ceil((avgCPUBurst *1000))/1000) << " ms" << endl;
+    SimOut << "-- CPU-bound average I/O burst time: " << (ceil((CPUavgIOBurst *1000))/1000) << " ms" << endl;
+    SimOut << "-- I/O-bound average I/O burst time: " << (ceil((IOavgIOBurst *1000))/1000) << " ms" << endl;
+    SimOut << "-- overall average I/O burst time: " << (ceil((avgIOBurst *1000))/1000) << " ms" << endl;
     SimOut.close();
 }
