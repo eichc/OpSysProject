ee13b52fe40c733297c943cd3a2b6c852711e34b
Satyam Patel <117528774+SatyamPatelCode@users.noreply.github.com>
2024-07-24 13:38:21 -0400

    Update printSimout.cpp

--- part1/printSimout.cpp
+++ part1/printSimout.cpp
@@ -14,7 +14,7 @@ void printSimout(int p, int CPUp, int IOp, double CPUavgCPUBurst, double IOavgCP
     SimOut << "-- number of CPU-bound processes: " << CPUp << endl;
     SimOut << "-- number of I/O-bound processes: " << IOp << endl;
     SimOut << "-- CPU-bound average CPU burst time: " << (ceil((CPUavgCPUBurst *1000))/1000) <<" ms" << endl;
-    SimOut << "-- IO-bound average CPU burst time: " <<  (ceil((IOavgCPUBurst *1000))/1000) <<" ms" << endl;
+    SimOut << "-- I/O-bound average CPU burst time: " <<  (ceil((IOavgCPUBurst *1000))/1000) <<" ms" << endl;
     SimOut << "-- overall average CPU burst time: " << (ceil((avgCPUBurst *1000))/1000) << " ms" << endl;
     SimOut << "-- CPU-bound average I/O burst time: " << (ceil((CPUavgIOBurst *1000))/1000) << " ms" << endl;
     SimOut << "-- I/O-bound average I/O burst time: " << (ceil((IOavgIOBurst *1000))/1000) << " ms" << endl;
